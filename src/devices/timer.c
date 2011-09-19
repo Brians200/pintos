30a0a2f75a70fae1c802ed683c2b34c4cc52ae5f
Brandon Davis <davisb@ksu.edu>
2011-09-18 19:29:54 -0500

    stashing changes

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -94,10 +94,12 @@ timer_sleep (int64_t ticks)
   ASSERT (intr_get_level () == INTR_ON);
   struct semaphore sema;
   sema_init(&sema,0);
-  sema_down(&sema);
-  
-  while (timer_elapsed (start) < ticks);
+  //printf("sema_int\n");
   sema_up(&sema);
+  //printf("sema_up\n");
+  while (timer_elapsed (start) < ticks);
+  sema_down(&sema);
+  //printf("sema_down\n");
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
