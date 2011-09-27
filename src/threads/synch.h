873a0fef54ae26470fdac566c659c039a7d6463e
Brian Sweeney <brians200@gmail.com>
2011-09-27 14:01:56 -0500

    Added method declaration to synch.h for donate_priority

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -41,6 +41,11 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
+
+//Our methods
+void donate_priority(struct thread *donor, struct thread *donee, struck lock*lock);
+//
+
 /* Optimization barrier.
 
    The compiler will not reorder operations across an
